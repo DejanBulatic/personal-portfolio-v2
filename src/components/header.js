98b43a4ffb07cb21f11259102d999997a32f279a
dejan bulatic <dejanbulatic@outlook.com>
2020-02-15 00:15:27 +0100

    navigation in place

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 const Header = () => {
     return (
         <header>
-            <p>This is the header</p>
+            <div className="container">
+                <nav>
+                    <ul>
+                        <li><Link to="#">Home</Link></li>
+                        <li><Link to="#">About</Link></li>
+                        <li><Link to="#">Projects</Link></li>
+                        <li><Link to="#">Video</Link></li>
+                        <li><Link to="#">Contact</Link></li>
+                    </ul>
+                </nav>
+            </div>
         </header>
     )
 }
