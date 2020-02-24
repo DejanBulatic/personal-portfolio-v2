7d8adc270b2caa76dc9b81f280014bb927206889
dejan bulatic <dejanbulatic@outlook.com>
2020-02-24 20:01:58 +0100

    about section complete

--- src/components/about.js
+++ src/components/about.js
@@ -3,9 +3,51 @@ import React from 'react'
 const About = () => {
     return (
         <section>
-            <h1>This is the About section</h1>
-        </section>
-    )
-}
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-md-4">
+                        <aside className="header-wraper">
+                            <h4 className="section-header">Background</h4>
+                        </aside>
+                    </div>
+                    <div className="col-12 col-md-8">
+                        <article>
+                            <p className="primary-text">
+                                I have 2 years of experience developing and
+                                designing software for the web, from simple landing
+                                pages to progressive web applications
 
+                            </p>
+                            <p className="primary-text">
+                                Currently in my last semester of the awesome 1 year
+                                <b> javascript development program </b>
+                                at the <a href="https://www.it-akademija.com/"><nobr>IT Academy</nobr></a>
+                                where I have improved my knowlege and developed a
+                                strong foundation in the latest technologies
+                                that enables me to create software that not only functions
+                                efficiently under the hood, but also provides intuitive,
+                                pixel-perfect user experiences.
+                            </p>
+                            <p className="primary-text">
+                                I aspire towards a career at a exiting company
+                                where I can progress and challange my skills.{"\n"}
+                                    Considering work an ongoing education, I'm always 
+                                    looking for opportunities to work with those who are willing 
+                                    to share their knowledge as much as I want to learn. At the end 
+                                    of the day, my primary goal is to create something beautiful 
+                                    with people that bring out the best in me.
+                            </p>
+                                <p className="primary-text">
+                                    <b>When I'm not in front of a computer screen</b>, I'm
+                                    probably out joging or crossing off another item on my bucket
+                                    list.
+                            </p>
+                        </article>
+                    </div>
+                    </div>
+                </div>
+        </section>
+            )
+        }
+        
 export default About
