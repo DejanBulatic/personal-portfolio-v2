145087b2c5e489365602b4d9a6f7339291559686
dejan bulatic <dejanbulatic@outlook.com>
2020-02-24 17:40:56 +0100

    test-run

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import WavingHand from '../images/waving-hand.png'
+import DejanProfileImage from '../images/dejan-round.png'
 
 const Hero = () => {
     return (
@@ -7,16 +9,17 @@ const Hero = () => {
                 <div className="row">
                     <div className="col-12 col-md-7 helloContent">
                         <div>
-                            <h1 className="hello">Hello!</h1>
+                            <h1 className="hello">Hello! <span className="waveHand"><img src={WavingHand} /></span></h1>
                             <h1 className="helloText">
-                                I'm Dejan Bulatic, a software engineer
+                                I'm
+                                Dejan Bulatic, a software engineer
                             who enjoys developing exeptional, high-quality websites &amp; applications.
                             </h1>
                         </div>
                     </div>
-                    <div className="col-12 col-md-5">
+                    <div className="col-12 col-md-5 heroImage">
                         <figure>
-
+                            <img src={DejanProfileImage} alt="Author" />
                         </figure>
                     </div>
                 </div>
